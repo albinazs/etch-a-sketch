0e84eb8c2b5428f5cf0faa187858cfd9e23f0209
Albina Starykova <albina.starikova@gmail.com>
2022-06-17 16:59:22 +0300

    Add functions to buttons to change painting mode

--- script.js
+++ script.js
@@ -1,55 +1,70 @@
-let color = 'black';
+let currentMode = 'default';
+let density = 32;
+
+//CREATING GRID
 const container = document.querySelector('.container');
 
-function createGrid (rows, columns) {
-    container.style.setProperty('--grid-rows', rows)
-    container.style.setProperty('--grid-columns', columns)
-    for(let i = 0; i < rows * columns; i++) {
+function createGrid (density) {
+    console.log(currentMode);
+    container.style.gridTemplateColumns = `repeat(${density}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${density}, 1fr)`;
+    for(let i = 0; i < density * density; i++) {   
+        console.log(currentMode);    
             const gridDiv = document.createElement('div');
             container.appendChild(gridDiv);
             gridDiv.classList.add('grid-divs');
+            gridDiv.addEventListener('mouseover', painting);
+            
         }
-    handleMouseover();
     }
 
-function handleMouseover () {
-    const gridDivs = Array.from(document.querySelectorAll('.grid-divs'));
-    gridDivs.forEach(div => div.addEventListener('mouseover', painting));
-}
+createGrid(density);
 
 function painting(e) {
-    e.target.style.backgroundColor = color;
+    console.log(currentMode);
+    if(currentMode === 'rainbow') {
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (currentMode === 'default') {
+        e.target.style.backgroundColor = 'black';
+    } else if (currentMode === 'eraser') {
+        e.target.style.backgroundColor = 'white';
     }
-
-function getRandom () {
-    const random = Math.floor(Math.random() * 54) + 10;
-    createGrid(random, random);
-}
-getRandom();
-
-function randomColor () {
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
-    return "rgb(" + r + "," + g + "," + b + ")";    
 }
 
-const eraser = document.querySelector('.eraser');
-eraser.addEventListener('click', () => color = 'white');
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('mouseover', () => button.classList.add('scale')));
+buttons.forEach(button => button.addEventListener('mouseout', () => button.classList.remove('scale')));
 
-const rainbow = document.querySelector('.rainbow');
-rainbow.addEventListener('click', () => color = "reel");
-
-const clear = document.querySelector('.clear');
-clear.addEventListener('click', clearAll);
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearAll);
 
 function clearAll () {
     const gridDivs = Array.from(document.querySelectorAll('.grid-divs'));
     gridDivs.forEach(div => div.style.backgroundColor = 'white');
 }
 
-//create default function
+const defaultButton = document.querySelector('.default');
+const rainbowButton = document.querySelector('.rainbow');
+const eraserButton = document.querySelector('.eraser');
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('mouseover', () => button.classList.add('scale')));
-buttons.forEach(button => button.addEventListener('mouseout', () => button.classList.remove('scale')));
+defaultButton.addEventListener('click', () => currentMode = 'default');
+rainbowButton.addEventListener('click', () => currentMode = 'rainbow');
+eraserButton.addEventListener('click', () => currentMode = 'eraser');
+
+const slider = document.querySelector('.slider');
+const slideOutput = document.querySelector('.outputValue');
+slideOutput.textContent = slider.value;
+let input = slideOutput.textContent;
+
+slider.oninput = function () {
+    slideOutput.textContent = this.value;
+    input = slideOutput.textContent;
+    console.log(input);
+    createGrid(input);
+}
+
+
+
